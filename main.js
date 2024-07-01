44ff9229d949727e2a74eddc5c78151a4c544f56
Lubna <muspaksamsu@gmail.com>
2024-07-01 23:03:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// They think of something you could store in a TypeScript Object. 
+//Write a program that creates Objects containing these items.
+var itCourse = {
+    name: "typescript and javascript",
+    location: "Governer House Sindh",
+    onsitestudents: 50000
+};
+console.log(itCourse);
---
+++ main.ts
@@ -0,0 +1,18 @@
+// They think of something you could store in a TypeScript Object. 
+//Write a program that creates Objects containing these items.
+
+interface itCourse{
+    name :string
+    location :string
+    onsiteStudent :number
+
+}
+
+let itCourse = {
+    name:"typescript and javascript",
+    location: "Governer House Sindh",
+    onsitestudents: 50000
+
+};
+console.log(itCourse);
+
